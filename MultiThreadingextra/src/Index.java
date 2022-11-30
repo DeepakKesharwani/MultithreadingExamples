c5ce9373e767a054cc162261f9ff37c5199ba989
deepakkumark <erdeepakkesharwani1997@gmail.com>
2022-11-30 17:10:00 +0530

    third  commit

--- MultiThreadingextra/src/Index.java
+++ MultiThreadingextra/src/Index.java
@@ -3,6 +3,7 @@ public class Index {
 
 	public static void main(String[] args) {
 		System.out.println("I am index page ");
+		System.out.println("I have done the implementation on push and pull ");
 	}
 
 }
