3e79a23704e1c215e60f6f0beb5d3124f6c4bbc5
Deepak Kumar Kesharwani <44540414+DeepakKesharwani@users.noreply.github.com>
2022-11-30 17:45:00 +0530

    Update Login.java

--- MultiThreadingextra/src/Login.java
+++ MultiThreadingextra/src/Login.java
@@ -3,7 +3,7 @@ public class Login {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am logout page ");
+		System.out.println("I am login page ");
 	}
 
 }
